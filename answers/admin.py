9a0eb4c8d6d2adc61f3fd41ac013bbc984aa06f1
davecaputo <davecaputo@hotmail.com>
2018-06-18 14:11:12 +0100

    Altered Answer model to allow choice=0 as default. Answers automatically created when AnswerSet created

--- answers/admin.py
+++ answers/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Answer, AnswerSet
+
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
@@ -9,5 +11,13 @@ class AnswerAdmin(admin.ModelAdmin):
 
 @admin.register(AnswerSet)
 class AnswerSetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'questionnaire', 'get_answers']
 
+    def get_answers(self, obj):
+        ans_list = [f'<span style="display:block">'
+                    f'Q{a.question.order + 1}={a.answer}</span>' for a in
+                    obj.answer_set.all()]
+
+        answers = ''.join(ans_list)
+
+        return format_html(answers)
--- answers/models.py
+++ answers/models.py
@@ -15,22 +15,40 @@ class AnswerSet(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        
+        created = True if not self.pk else False
+        super().save(*args, **kwargs)
+        # Create answerset
+        if created:
+            for question in self.questionnaire.question_set.all():
+                Answer.objects.create(
+                    answerset=self,
+                    question=question,
+                )
+
 
 class Answer(models.Model):
 
     ANSWER_CHOICES = [
+        (0, '0'),
         (1, '1'),
         (2, '2'),
         (3, '3'),
         (4, '4'),
         (5, '5')
     ]
+
     answerset = models.ForeignKey(AnswerSet, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer = models.IntegerField(choices=ANSWER_CHOICES, blank=True, null=True)
+
+    answer = models.IntegerField(choices=ANSWER_CHOICES,
+                                 blank=True,
+                                 null=True,
+                                 default=0)
 
     def __str__(self):
         return f'{self.answerset}: Q{self.question.order  + 1} = {self.answer}'
 
     class Meta:
-        ordering = ('question__order',)
+        ordering = ('answerset', 'question__order',)
