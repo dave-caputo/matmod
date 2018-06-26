7c052dcc93bd65d615090b85f93320b2d3c11e80
davecaputo <davecaputo@hotmail.com>
2018-06-26 12:51:15 +0100

    base.html set up. Qre update form enabled.

--- questionnaires/urls.py
+++ questionnaires/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import QreCreateView, QreListView
+from . import views
 
 app_name = 'qres'
 urlpatterns = [
-    path('create/', QreCreateView.as_view(), name='create'),
-    path('list/', QreListView.as_view(), name='list'),
+    path('create/', views.QreCreateView.as_view(), name='create'),
+    path('list/', views.QreListView.as_view(), name='list'),
+    path('detail/<int:pk>/', views.QreDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', views.QreUpdateView.as_view(), name='update')
 ]
--- questionnaires/views.py
+++ questionnaires/views.py
@@ -1,16 +1,30 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
 from .models import Qre
 
 
-class QreCreateView(CreateView):
+class QreCreateView(generic.CreateView):
     model = Qre
     template_name = 'qres/create.html'
     fields = ['name']
     success_url = reverse_lazy('qres:create')
 
 
-class QreListView(ListView):
+class QreListView(generic.ListView):
     model = Qre
     template_name = 'qres/list.html'
+
+
+class QreDetailView(generic.DetailView):
+    model = Qre
+    template_name = 'qres/detail.html'
+
+
+class QreUpdateView(generic.UpdateView):
+    model = Qre
+    template_name = 'qres/update.html'
+    fields = ['name']
+
+    def get_success_url(self):
+        return reverse('qres:update', kwargs={'pk': self.kwargs['pk']})
