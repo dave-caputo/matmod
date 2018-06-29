6086a60307629291c39b00ba1f876c74e7d9d3bf
davecaputo <davecaputo@hotmail.com>
2018-06-29 12:08:09 +0100

    Qre section CREATE operation set up.

--- questionnaires/urls.py
+++ questionnaires/urls.py
@@ -4,9 +4,20 @@ from . import views
 
 app_name = 'qres'
 urlpatterns = [
+
+    # Questionnaires...
     path('create/', views.QreCreateView.as_view(), name='create'),
     path('list/', views.QreListView.as_view(), name='list'),
     path('detail/<int:pk>/', views.QreDetailView.as_view(), name='detail'),
     path('update/<int:pk>/', views.QreUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', views.QreDeleteView.as_view(), name='delete'),
+
+    # Sections...
+    path('questionnaire-<int:qre_id>/create-section/',
+         views.SectionCreateView.as_view(),
+         name='section_create'),
+    path('questionnaire-<int:qre_id>/section-list/',
+         views.SectionListView.as_view(),
+         name='section_list'),
+
 ]
--- questionnaires/views.py
+++ questionnaires/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
-from .models import Qre
+from .forms import QreSectionForm
+from .models import Qre, Section
 
 
 class QreCreateView(generic.CreateView):
@@ -34,3 +36,31 @@ class QreDeleteView(generic.DeleteView):
     model = Qre
     template_name = 'qres/delete.html'
     success_url = reverse_lazy('dashboard:index')
+
+
+class SectionCreateView(generic.CreateView):
+    model = Section
+    template_name = 'qres/section_create.html'
+    form_class = QreSectionForm
+    success_url = reverse_lazy('qres:section_create')
+
+    def get_initial(self):
+        qre = get_object_or_404(Qre, id=self.kwargs['qre_id'])
+        return {'qre': qre}
+
+    from django.urls import reverse_lazy
+
+    def get_success_url(self):
+        return reverse('qres:section_create',
+                       kwargs={'qre_id': self.kwargs['qre_id']})
+
+
+class SectionListView(generic.ListView):
+    model = Section
+    template_name = 'qres/section_list.html'
+
+    def get_queryset(self):
+        return (
+            Section.objects.select_related('qre')
+            .filter(qre__id=self.kwargs['qre_id'])
+        )
