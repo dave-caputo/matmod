7c753d83bbaba34bb7f7defe052b8ddbf3db294d
davecaputo <davecaputo@hotmail.com>
2018-07-12 21:27:58 +0100

    Removed commented lines from form.

--- answers/forms.py
+++ answers/forms.py
@@ -7,7 +7,6 @@ class AnswerForm(forms.ModelForm):
     answer = forms.ChoiceField(
         choices=[(0, 'No response'), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)],
         widget=forms.RadioSelect())
-    # question_string = forms.CharField(max_length=255, widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,33 +14,4 @@ class AnswerForm(forms.ModelForm):
 
     class Meta:
         model = Answer
-        # fields = ['assessment', 'question', 'answer']
-        fields = ['id', 'answer']
-        widgets = {
-            # 'assessment': forms.HiddenInput(),
-            # 'question': forms.HiddenInput(),
-            # 'question_string': forms.HiddenInput()
-        }
-
-
-# def get_answer_formset():
-
-#     return forms.inlineformset_factory(
-#         Answer,
-#         form=AnswerForm,
-#         # fields=['assessment', 'question', 'answer'],
-#         fields=['id', 'answer'],
-#         extra=0,
-#         max_num=0
-#     )
-
-
-# def get_answer_formset():
-
-#     return forms.modelformset_factory(
-#         form=AnswerForm,
-#         # fields=['assessment', 'question', 'answer'],
-#         # fields=['id', 'answer'],
-#         extra=0,
-#         max_num=0
-#     )
+        fields = ['answer']
