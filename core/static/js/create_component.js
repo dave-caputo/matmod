63a309f5c2d19a63a2ee8bfbdb49a978d546985d
davecaputo <davecaputo@hotmail.com>
2018-10-22 20:41:00 +0100

    Split static files between dashboard/core

--- core/static/js/create_component.js
+++ core/static/js/create_component.js
@@ -1,4 +1,4 @@
-function objectCreateComponent(config){
+function createObject(config){
     // A reusable component to create objects on page.
 
     /*================================
@@ -92,35 +92,3 @@ function objectCreateComponent(config){
         });
     });
 }
-
-$(document).ready(function(){
-    var clientConfig = {
-        objectClass: 'client',
-        actionUrl: $('#page_urls').data('clientCreateUrl'),
-        listUrl: $('#page_urls').data('clientListUrl'),
-        formDisplayBtn: '#client_create_display',
-        formDisplayDiv: '#client_create_div',
-        listDisplayDiv: '#client_list_div',
-        formSubmitBtn: '#client_create_btn',
-        createForm: '#client_create_form',
-        actionEvent: 'clientCreated',
-        hiddenLabels: true,
-    };
-    setCreateComponent(clientConfig);
-
-    var qreConfig = {
-        objectClass: 'questionnaire',
-        actionUrl: $('#page_urls').data('qreCreateUrl'),
-        listUrl: $('#page_urls').data('qreListUrl'),
-        formDisplayBtn: '#qre_create_display',
-        formDisplayDiv: '#qre_create_div',
-        listDisplayDiv: '#qre_list_div',
-        formSubmitBtn: '#qre_create_btn',
-        createForm: '#qre_create_form',
-        actionEvent: 'qreCreated',
-        hiddenLabels: true,
-    };
-    setCreateComponent(qreConfig);
-
-});
-
