d5b22e2262768b03a05b7274f5f7ca23d3ce9b3b
davecaputo <davecaputo@hotmail.com>
2018-11-30 23:31:58 +0000

    Updated models, data and recreated migrations

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # 3rd party apps
     'crispy_forms',
+    'mptt',
     'ordered_model',
 
     # Project apps
--- orgs/models.py
+++ orgs/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+from mptt.models import MPTTModel, TreeForeignKey
+
 from .countries import COUNTRIES
 
 
-class Org(models.Model):
+class Org(MPTTModel):
     name = models.CharField(max_length=55)
     country = models.CharField(max_length=255, choices=COUNTRIES, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = 'name'
+
+    def __str__(self):
+        return self.name
