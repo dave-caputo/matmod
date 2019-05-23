bec6b7e10c2e93cccdee541dec127a163c4a3770
David Caputo <david.caputo@festicket.com>
2019-05-23 15:34:31 -0500

    Update local settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,9 +8,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'matmod_dev',
-        'USER': 'davecaputo',
-        'PASSWORD': 'password',
+        'NAME': 'matmod',
+        'USER': 'matmod',
+        'PASSWORD': 'matmod',
         'HOST': 'localhost',
         'PORT': '5432',
     }
