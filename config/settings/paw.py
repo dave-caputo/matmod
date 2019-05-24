71a8f3853507d4a8852fd4340e6880c872eccf3f
David Caputo <david.caputo@festicket.com>
2019-05-24 15:22:28 -0500

    Update requirements (no postgres), and move data upload to mgt command

--- config/settings/paw.py
+++ config/settings/paw.py
@@ -1,9 +1,15 @@
 # Settings for PythonAnywhere
+
+from django.db.backends.mysql.base import DatabaseWrapper
+
 from .base import *
-import os
+
+DatabaseWrapper.data_types['DateTimeField'] = 'datetime'  # fix for MySQL 5.5
 
 
 DEBUG = False
+
+ALLOWED_HOSTS = ['davecaputo.pythonanywhere.com']
 
 # Mysql database configuration
 DATABASES = {
@@ -13,7 +19,16 @@ DATABASES = {
         'USER': 'davecaputo',
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': 'davecaputo.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ core/management/commands/load_test_data.py
@@ -0,0 +1,142 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
+from django.core.management.base import BaseCommand
+
+from clients.models import Client
+from core.admin import CustomAdminSite
+from orgs.countries import COUNTRIES
+from orgs.models import Org
+from questionnaires.models import Qre
+from questions.models import Question
+from sections.models import Section
+
+
+class Command(BaseCommand):
+    help = 'Populates database with test data'
+
+    def handle(self, *args, **options):
+        # Admin site
+
+        site = Site.objects.get_current()
+        site.name = 'Tech Modelling'
+        site.save()
+
+        CustomAdminSite.name = site.name
+
+        # Orgs
+
+        matmod = Org.objects.create(name='MatMod Corp', country=COUNTRIES.GBR)
+        test_org = Org.objects.create(name='Test Org', country=COUNTRIES.GBR, parent=matmod)
+
+        # User
+
+        User = get_user_model()
+
+        if settings.DEBUG:
+            User.objects.create_superuser(
+                username='superuser',
+                email='superuser@example.com',
+                password='mypassword',
+                org=matmod,
+            )
+
+        User.objects.create_user(
+            username='test_admin1',
+            email='test_admin1@example.com',
+            password='somepassword',
+            is_staff=True,
+            org=test_org,
+        )
+
+        User.objects.create_user(
+            username='test_admin2',
+            email='test_admin2@example.com',
+            password='somepassword',
+            is_staff=True,
+            org=test_org,
+        )
+
+        # Client
+
+        Client.objects.create(name='ACME LTD', org=test_org)
+        Client.objects.create(name='SKYNET CORP', org=test_org)
+
+        # Questionnaire
+
+        fgmt = Qre.objects.create(name='Fruit Gourmet Maturity Model', org=test_org)
+
+        tropical = Section.objects.create(
+            qre=fgmt,
+            name='Tropical Fruits'
+        )
+
+        berries = Section.objects.create(
+            qre=fgmt,
+            name='Berries'
+        )
+
+        frozen_fruit = Section.objects.create(
+            qre=fgmt,
+            name='Frozen fruit'
+        )
+
+        Question.objects.create(
+            section=tropical,
+            question='You eat at 5 mangoes a week.',
+            choice_text_1='I eat zero mangoes. I hate them.',
+            choice_text_2='I have a mango once in a blue moon.',
+            choice_text_3='I eat at least 3 mangoes a week.',
+            choice_text_4='I eat 4 mangoes a week and feel great.',
+            choice_text_5='I eat 5 or more mangoes a day because I can\'t have enough of them.',
+        )
+
+        Question.objects.create(
+            section=tropical,
+            question='You climb palm trees to get coconuts at least once a day.',
+            choice_text_1='I climb palm trees but avoid the coconuts.',
+            choice_text_2='I eat coconuts once a month but rarely climb the palm tree.',
+            choice_text_3='I climb every other day.',
+            choice_text_4='I climb every day, eat my coconuts and feel no shame about it.',
+            choice_text_5='Nobody climbs palm trees to get coconuts more than me every a day.',
+        )
+
+        Question.objects.create(
+            section=berries,
+            question='You add raspberries to your cereal every morning.',
+            choice_text_1='I only eat sausage and eggs for breakfast.',
+            choice_text_2='I dont have anything against raspberries in my cereal but I rarely have them.',
+            choice_text_3='I add raspberries with my cereal once a month.',
+            choice_text_4='I mostly add rasbperries but sometimes I need to change and add blueberries instead.',
+            choice_text_5='I only know the meanining of breakfast as having cereal with raspberries.',
+        )
+
+        Question.objects.create(
+            section=berries,
+            question='You have a blueberry smoothie when everyone else is ordering coffee.',
+            choice_text_1=' I love coffee, and never heard about blueberries.',
+            choice_text_2='I tend to prefer coffee, but sometimes go for blueberry smoothie.',
+            choice_text_3='I alternate between blueberry smoothies and coffee.',
+            choice_text_4='I prefer blueberry smoothies but sometimes give up due to peer pressure',
+            choice_text_5='I absolutely go for blueberry smoothie. I don\'t care about coffee',
+        )
+
+        Question.objects.create(
+            section=frozen_fruit,
+            question='I always have a fruit sorbet as dessert at dinner.',
+            choice_text_1='The thought of a fruit sorbet makes me sick.',
+            choice_text_2='I had a sorbet once and liked it, but then I never remember to take one again.',
+            choice_text_3='I have a fruit sorbet once a month.',
+            choice_text_4='I really like sorbets, but sometimes can\'t resist a creme brulee.',
+            choice_text_5='Fruit sorbet for dessert is my religion.',
+        )
+
+        Question.objects.create(
+            section=frozen_fruit,
+            question='You use frozen fruit as ice cubes in your drinks.',
+            choice_text_1='Never. I wouldn\'t change the .',
+            choice_text_2='Sometimes, but only when I run out of ice cubes.',
+            choice_text_3='I like it but only when drinking water.',
+            choice_text_4='I prefer frozen fruit but wouldn\'t say no to ice cubes.',
+            choice_text_5='Always. In my house is forbidden to buy anything else.',
+        )
